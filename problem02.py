3c04a5f3f40228ed478e97b99f098aa25e066400
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2021-12-03 10:02:44 -0600

    puzzle-2 complete

--- problem02.py
+++ problem02.py
@@ -6,7 +6,7 @@ Reading in from the text file
 
 Loading the numbers in the file in an array
 
-Algorithm --> 
+Algorithm --> Simple parsing and if-else statements
 '''
 
 INPUT_FILE_PATH = "input/prob2.input.txt"
@@ -15,23 +15,25 @@ def parse_input(filepath):
     with open(filepath) as f:
         lines = f.readlines()
     
-    # new_format = [''] * len(lines)
     depth = 0
     horzp = 0
-    # for idx, l in enumerate(lines):
+    aim = 0
+    
     for line in lines:
         command, value = line.split()
 
         if command == 'forward':
             horzp += int(value)
+            depth += aim * int(value)
         elif command == 'up':
-            depth -= int(value)
+            aim -= int(value)
         elif command == 'down':
-            depth += int(value)
+            aim += int(value)
 
     print("Horizontal Position: ", horzp)
     print("Depth: ", depth)  
-    
+    print("Aim: ", aim)
+
     return depth * horzp
 
 def main():
