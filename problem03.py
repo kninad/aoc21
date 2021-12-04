b8169f1615d33e4b52964c85b02d312884708346
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2021-12-04 00:44:19 -0600

    prob3 in progress

--- problem03.py
+++ problem03.py
@@ -11,10 +11,8 @@ Algorithm:
 Brute Force approach -- for K-bit number, simply keep track of counts of 1 and 0 for
 that bit position.
 '''
-
 import numpy as np
 INPUT_FILE_PATH = "input/prob3.input.txt"
-
 
 def parse_input(filepath):
     with open(filepath) as f:
@@ -23,42 +21,41 @@ def parse_input(filepath):
     lines = [l.strip() for l in lines]
     return lines
 
-def update_counts(bitmask: str, c_one, c_zero) -> None:
-    for i, c in enumerate(bitmask):
-        if c == '1':
-            c_one[i] += 1
-        else:
-            c_zero[i] += 1
+def convert_to_np(str_array):
+    N = len(str_array)
+    bit_length = len(str_array[0])
+    data = np.zeros((N, bit_length))
+    for i, bitmask in enumerate(str_array):
+        # for j, c in enumerate(bitmask):
+        #     if c == '1':
+        #         data[i][j] = 1
+        data[i] = np.array([int(c) for c in bitmask])
+    return data
 
+def get_counts(data) -> None:
+    N = len(data)
+    one_counts = np.sum(data, axis=0)
+    zero_counts =  N - one_counts
+    return one_counts, zero_counts
 
-def bit_calc(array):
+def get_dominant_bits(array):
     bit_length = len(array[0])
-    one_bits =  np.zeros(bit_length)
-    zero_bits = np.zeros(bit_length)
+    c_one, c_zero =  get_counts(array)
+    dominant = np.ones(bit_length)
+    dominant[c_zero > c_one] = 0
+    return dominant
 
-    for bitmask in array:
-        update_counts(bitmask, one_bits, zero_bits)
-    
-    gamma = ['0'] * bit_length
-    epsilon = ['0'] * bit_length
+def oxygen_rating(array, dominant):
+    pass
 
-    for i in range(bit_length):
-        if one_bits[i] > zero_bits[i]:
-            gamma[i] = '1'
-        else:
-            epsilon[i] = '1'
-    
-    gamma_val = int('0b' + ''.join(gamma), 2)
-    epsilon_val = int('0b' + ''.join(epsilon), 2)
-
-    return gamma_val * epsilon_val
-
+def co2_rating(array, dominant):
+    pass
 
 
 
 
 def main():
-    print("Answer: ", bit_calc(parse_input(INPUT_FILE_PATH)))
+    print("Answer: ")
 
 
 if __name__ == "__main__":
